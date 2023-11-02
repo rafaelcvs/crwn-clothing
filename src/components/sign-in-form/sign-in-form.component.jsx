b2379fc8d1725f60bc31f1b5979160c81ecddc8c
rafaelcvs <rafaelcvs18@hotmail.com>
2023-11-02 18:21:15 +1000

    Created auto state change and auth listener

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import FormInput from "../form-input/form-input.componet";
 import Button from "../button/button.component";
-import { UserContext } from "../../contexts/user.context";
+
 
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
@@ -16,16 +16,14 @@ const defaultFormFields = {
 const SignInForm = () => {
     const [ formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
-
-    const { setCurrentUser } = useContext(UserContext);
-
+   
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     };
 
     const signInWithGoogle = async () => {
-        const {user} = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user);
+        await signInWithGooglePopup();
+        
     };
 
     const handleSubmit = async (event) => {
@@ -33,7 +31,6 @@ const SignInForm = () => {
         
         try {
             const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
             resetFormFields();
         } catch (error) {
             if (error.code === "auth/invalid-login-credentials")
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,5 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import FormInput from "../form-input/form-input.componet";
-import { UserContext } from "../../contexts/user.context";
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
@@ -18,8 +17,6 @@ const SignUpForm = () => {
     const [ formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    const { setCurrentUser } = useContext(UserContext);
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
@@ -36,8 +33,6 @@ const SignUpForm = () => {
                 email,
                 password
             );
-
-            setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();

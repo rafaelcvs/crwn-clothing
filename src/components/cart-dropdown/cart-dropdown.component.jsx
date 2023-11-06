22e4f35ed59decf1d9c292e62e43ac4169fbcbbe
rafaelcvs <rafaelcvs18@hotmail.com>
2023-11-07 07:49:38 +1000

    Adding add to card and Cart count and designs

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component'
+ 
+import './cart-dropdown.styles.scss'
 
-import './cart-dropdown.styles.scss';
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
 
-const CartDropdown = () => (
-  <div className='cart-dropdown-container'>
-    <div className='cart-items' />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((item) => (
+            <CartItem key={item.id} cartItem={item} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;

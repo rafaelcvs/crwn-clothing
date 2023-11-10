891994ce0f9d7eabfd433715907e5d9902a4c24c
rafaelcvs <rafaelcvs18@hotmail.com>
2023-11-10 18:09:02 +1000

    created category preview, categories previews etc.

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+
+import { gerCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({})
+
+    useEffect(()=> {
+        const getCategoriesMap = async () => {
+            const categoryMap = await gerCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        }
+
+        getCategoriesMap();
+    }, []);
+
+    const value = {categoriesMap}
+    return (
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+    )
+}

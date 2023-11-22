24869ee82112a9a3361b21c33a606c62ddfe03a0
rafaelcvs <rafaelcvs18@hotmail.com>
2023-11-22 14:57:06 +1000

    finished code migration to redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './contexts/cart.context';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
@@ -12,10 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+      <BrowserRouter>        
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,10 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
-import { CartContext } from "../../contexts/cart.context";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -12,8 +12,7 @@ import { NavigationContainer, NavLinks, NavLink, LogoContainer } from "./navigat
 
 const Navigation = () => {
     const currentUser = useSelector(selectCurrentUser);
- 
-    const { isCartOpen } = useContext(CartContext)
+    const isCartOpen = useSelector(selectIsCartOpen);
 
     return (
         <Fragment>
